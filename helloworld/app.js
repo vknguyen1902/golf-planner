03c8d1c2324639e543e0991bb1448168ee56d4c6
金田傑 <kanedasuguru@mba.local>
2022-11-22 22:22:32 +0900

    my first typescript task

---
+++ helloworld/app.js
@@ -0,0 +1,7 @@
+var message = 'Hello Kaneda Bibi!';
+// heading 1 element
+var heading = document.createElement('h1');
+heading.textContent = message;
+// add heading to the document
+document.body.appendChild(heading);
+// console.log(message);
---
+++ helloworld/app.ts
@@ -0,0 +1,8 @@
+let message: string = 'Hello Kaneda Bibi!';
+// heading 1 element
+let heading = document.createElement('h1');
+heading.textContent = message;
+
+// add heading to the document body
+document.body.appendChild(heading);
+// console.log(message);
